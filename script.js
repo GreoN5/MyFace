65dffff3dee815edc03b765be9def7b9065609aa
GreoN5 <georgi38@abv.bg>
2021-02-03 18:43:27 +0200

    Making smile stop and centering eyes

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
+const mouthSmile = document.getElementById("mouth");
+const centeredEyes = document.getElementById("eyes");
+
 function smile() {
-    const mouthSmile = document.getElementById("mouth");
+    clearClasses();
     mouthSmile.classList.add("smile");
+}
+
+function stopSmile() {
+    clearClasses();
+    mouthSmile.classList.add("stop-smile");
+}
+
+function centerEyes() {
+    centeredEyes.classList.add("centered-eyes");
+}
+
+function clearClasses() {
+    for (let i = 0; i < mouthSmile.classList.length; i++) {
+        if (mouthSmile.classList[i] != "mouth") {
+            mouthSmile.classList.remove(mouthSmile.classList[i]);
+        }
+    }
 }
